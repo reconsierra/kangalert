4b37e666e6e6fd903f7d4525be9456ab1f1bf5a3
Rec0n <offers@extensive.com.au>
2025-09-14 10:26:43 +1000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,15 +1,10 @@
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, firestore
 import json
 
-# Load Firebase credentials from Streamlit secrets
 try:
-    if not firebase_admin._apps:
-        firebase_credentials = st.secrets.firebase.json
-        cred = credentials.Certificate(json.loads(firebase_credentials))
-        firebase_admin.initialize_app(cred)
-        st.success("Firebase credentials loaded successfully!")
+    firebase_credentials = st.secrets.firebase.json
+    json.loads(firebase_credentials)
+    st.success("Firebase credentials loaded successfully!")
 except Exception as e:
     st.error(f"Error loading Firebase credentials: {e}")
     st.stop()
